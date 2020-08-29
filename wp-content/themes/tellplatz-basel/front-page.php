b43b42f01c170b70481c7eab6ac5e341f1c9f539
Samuel Rüegger <samuel@rueegger.me>
2020-08-29 23:05:36 +0200

    some changes

--- wp-content/themes/tellplatz-basel/front-page.php
+++ wp-content/themes/tellplatz-basel/front-page.php
@@ -20,7 +20,7 @@ if ( have_posts() ) : while ( have_posts() ) : the_post()
 			<source media="(min-width: 80em)" srcset="https://via.placeholder.com/1920x1080 1x, https://via.placeholder.com/3840x2160 2x">
 			<source media="(min-width: 40em)" srcset="https://via.placeholder.com/726x504 1x, https://via.placeholder.com/1452x1008 2x">
 			<source srcset="https://via.placeholder.com/333x386 1x, https://via.placeholder.com/666x772 2x">
-			<img loading="lazy" src="https://via.placeholder.com/333x386" alt="">
+			<img src="https://via.placeholder.com/333x386" alt="">
 		</picture>
 		<div class="scroll-down js_scroll_to" data-scrollto="#petition" role="button">
 			<span><i class="fal fa-chevron-double-up"></i>Mehr erfahren</span>
@@ -54,20 +54,26 @@ if ( have_posts() ) : while ( have_posts() ) : the_post()
 		<div class="container mt-45">
 			<div class="row">
 				<div class="column">
-					<h4 class="bold">Warum soll länger offen sein?</h4>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+					<div class="mobile-padding">
+						<h4 class="bold">Warum soll länger offen sein?</h4>
+						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+					</div>
 				</div>
 			</div>
 			<div class="row">
 				<div class="column">
-					<h4 class="bold">Was sind die Vorteile fürs Quartier?</h4>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+					<div class="mobile-padding">
+						<h4 class="bold">Was sind die Vorteile fürs Quartier?</h4>
+						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+					</div>
 				</div>
 			</div>
 			<div class="row">
 				<div class="column">
-					<h4 class="bold">Lorem Ipsum?</h4>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+					<div class="mobile-padding">
+						<h4 class="bold">Lorem Ipsum?</h4>
+						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+					</div>
 				</div>
 			</div>
 		</div>
--- wp-content/themes/tellplatz-basel/template-petition.php
+++ wp-content/themes/tellplatz-basel/template-petition.php
@@ -9,9 +9,14 @@ if ( have_posts() ) : while ( have_posts() ) : the_post()
 		<div class="row">
 			<div class="column">
 				<div class="mobile-padding">
-					<a href="<?php echo HOME_URI; ?>/#petition" target="_self" class="button"><i class="fas fa-pencil"></i>Petition jetzt unterschreiben</a>
-					<a href="#" target="_blank" class="button button-outline"><i class="fas fa-cloud-download-alt"></i>Herunterladen</a>
-					<div class="clearfix"></div>
+					<?php
+					$btn1 = get_field('petition_btn1');
+					$btn2 = get_field('petition_btn2');
+					$link_target1 = $btn1['target'] ? $btn1['target'] : '_self';
+					$link_target2 = $btn2['target'] ? $btn2['target'] : '_self';
+					?>
+					<a href="<?php echo $btn1['url']; ?>" target="<?php echo $link_target1; ?>" class="button"><i class="fas fa-pencil"></i><?php echo $btn1['title']; ?></a>
+					<a href="<?php echo $btn2['url']; ?>" target="<?php echo $link_target2; ?>" class="button button-outline"><i class="fas fa-cloud-download-alt"></i><?php echo $btn2['title']; ?></a>
 				</div>
 			</div>
 		</div>
