541390b1fae9bd3e93529e321debe3896b8910be
Samuel Rüegger <samuel@rueegger.me>
2020-08-30 00:05:57 +0200

    dynamic Front Page

--- wp-content/themes/tellplatz-basel/front-page.php
+++ wp-content/themes/tellplatz-basel/front-page.php
@@ -1,41 +1,46 @@
 <?php
 get_header();
-if ( have_posts() ) : while ( have_posts() ) : the_post()
+if ( have_posts() ) : while ( have_posts() ) : the_post();
+$first_image = get_field( 'front_first_image' );
+$second_image = get_field( 'front_second_img' );
+$third_image = get_field( 'front_third_img' );
 ?>
 <div id="front-header">
 	<div class="container">
 		<div class="row">
 			<div class="column">
 				<div class="first">
-					<h2>Tellplatz-Beizen sollen draussen am Abend länger offen bleiben!</h2>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-					<button class="button js_scroll_to" data-scrollto="#petition" type="button"><i class="fas fa-pencil"></i>Jetzt unterschreiben</button>
-					<button class="button button-outline js_scroll_to" data-scrollto="#questions" type="button"><i class="fas fa-question"></i>Argumente</button>
+					<h2><?php the_field( 'front_first_title' ); ?></h2>
+					<?php the_field( 'front_first_txt' ); ?>
+					<button class="button js_scroll_to" data-scrollto="#petition" type="button"><i class="fas fa-pencil"></i><?php the_field( 'front_first_btn1' ); ?></button>
+					<button class="button button-outline js_scroll_to" data-scrollto="#questions" type="button"><i class="fas fa-question"></i><?php the_field( 'front_first_btn2' ); ?></button>
 				</div>
 			</div>
 		</div>
 	</div>
 	<div class="second">
 		<picture>
-			<source media="(min-width: 80em)" srcset="https://via.placeholder.com/1920x1080 1x, https://via.placeholder.com/3840x2160 2x">
-			<source media="(min-width: 40em)" srcset="https://via.placeholder.com/726x504 1x, https://via.placeholder.com/1452x1008 2x">
-			<source srcset="https://via.placeholder.com/333x386 1x, https://via.placeholder.com/666x772 2x">
-			<img src="https://via.placeholder.com/333x386" alt="">
+			<source media="(min-width: 80em)" srcset="<?php echo $first_image['sizes']['fullwidth-desktop']; ?> 1x, <?php echo $first_image['sizes']['fullwidth-desktop-2x']; ?> 2x">
+			<source media="(min-width: 40em)" srcset="<?php echo $first_image['sizes']['fullwidth-tablet']; ?> 1x, <?php echo $first_image['sizes']['fullwidth-tablet-2x']; ?> 2x">
+			<source srcset="<?php echo $first_image['sizes']['fullwidth-mobile']; ?> 1x, <?php echo $first_image['sizes']['fullwidth-mobile-2x']; ?> 2x">
+			<img src="<?php echo $first_image['sizes']['fullwidth-mobile']; ?>" alt="<?php echo $first_image['alt']; ?>">
 		</picture>
 		<div class="scroll-down js_scroll_to" data-scrollto="#petition" role="button">
-			<span><i class="fal fa-chevron-double-up"></i>Mehr erfahren</span>
+			<span><i class="fal fa-chevron-double-up"></i><?php the_field( 'front_first_scrolldown' ); ?></span>
 		</div>
 	</div>
 </div>
 <div id="petition">
-	<?php tpb_page_title( 'Jetzt Unterschreiben' ); ?>
+	<?php tpb_page_title( get_field( 'front_second_maintitle' ) ); ?>
 	<div class="container mt-45">
 		<div class="row">
 			<div class="column">
 				<div class="mobile-padding two-columns">
-					<h4 class="bold">Unterstütze uns</h4>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-					<?php echo do_shortcode( '[gravityform id="1" title="false" description="false" ajax="true"]' ); ?>
+					<h4 class="bold"><?php the_field( 'front_second_title' ); ?></h4>
+					<?php
+					the_field( 'front_second_txt' );
+					echo do_shortcode( '[gravityform id="1" title="false" description="false" ajax="true"]' );
+					?>
 					<div id="sharing_container" class="mb-25"></div>
 				</div>
 			</div>
@@ -43,47 +48,31 @@ if ( have_posts() ) : while ( have_posts() ) : the_post()
 	</div>
 	<div class="full-image with-background-primary">
 		<picture>
-			<source media="(min-width: 80em)" srcset="https://via.placeholder.com/1920x1080 1x, https://via.placeholder.com/3840x2160 2x">
-			<source media="(min-width: 40em)" srcset="https://via.placeholder.com/726x504 1x, https://via.placeholder.com/1452x1008 2x">
-			<source srcset="https://via.placeholder.com/333x386 1x, https://via.placeholder.com/666x772 2x">
-			<img loading="lazy" src="https://via.placeholder.com/333x386" alt="">
+			<source media="(min-width: 80em)" srcset="<?php echo $second_image['sizes']['fullwidth-desktop']; ?> 1x, <?php echo $second_image['sizes']['fullwidth-desktop-2x']; ?> 2x">
+			<source media="(min-width: 40em)" srcset="<?php echo $second_image['sizes']['fullwidth-tablet']; ?> 1x, <?php echo $second_image['sizes']['fullwidth-tablet-2x']; ?> 2x">
+			<source srcset="<?php echo $second_image['sizes']['fullwidth-mobile']; ?> 1x, <?php echo $second_image['sizes']['fullwidth-mobile-2x']; ?> 2x">
+			<img loading="lazy" src="<?php echo $second_image['sizes']['fullwidth-mobile']; ?>" alt="<?php echo $second_image['alt']; ?>">
 		</picture>
 	</div>
 	<div id="questions">
-		<?php tpb_page_title( 'Fragen?' ); ?>
-		<div class="container mt-45">
-			<div class="row">
-				<div class="column">
-					<div class="mobile-padding">
-						<h4 class="bold">Warum soll länger offen sein?</h4>
-						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="column">
-					<div class="mobile-padding">
-						<h4 class="bold">Was sind die Vorteile fürs Quartier?</h4>
-						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="column">
-					<div class="mobile-padding">
-						<h4 class="bold">Lorem Ipsum?</h4>
-						<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-					</div>
-				</div>
-			</div>
-		</div>
+		<?php
+		tpb_page_title( get_field( 'front_third_title' ) );
+		if( have_rows( 'front_third_questions' ) ) {
+			echo '<div class="container mt-45">';
+			while( have_rows( 'front_third_questions' ) ) {
+				the_row();
+				get_template_part( 'templates/loop', 'questions' );
+			}
+			echo '</div>';
+		}
+		?>
 	</div>
 	<div class="full-image with-background">
 		<picture>
-			<source media="(min-width: 80em)" srcset="https://via.placeholder.com/1920x1080 1x, https://via.placeholder.com/3840x2160 2x">
-			<source media="(min-width: 40em)" srcset="https://via.placeholder.com/726x504 1x, https://via.placeholder.com/1452x1008 2x">
-			<source srcset="https://via.placeholder.com/333x386 1x, https://via.placeholder.com/666x772 2x">
-			<img loading="lazy" src="https://via.placeholder.com/333x386" alt="">
+			<source media="(min-width: 80em)" srcset="<?php echo $third_image['sizes']['fullwidth-desktop']; ?> 1x, <?php echo $third_image['sizes']['fullwidth-desktop-2x']; ?> 2x">
+			<source media="(min-width: 40em)" srcset="<?php echo $third_image['sizes']['fullwidth-tablet']; ?> 1x, <?php echo $third_image['sizes']['fullwidth-tablet-2x']; ?> 2x">
+			<source srcset="<?php echo $third_image['sizes']['fullwidth-mobile']; ?> 1x, <?php echo $third_image['sizes']['fullwidth-mobile-2x']; ?> 2x">
+			<img loading="lazy" src="<?php echo $third_image['sizes']['fullwidth-mobile']; ?>" alt="<?php $third_image['alt']; ?>">
 		</picture>
 	</div>
 </div>
---
+++ wp-content/themes/tellplatz-basel/templates/loop-questions.php
@@ -0,0 +1,8 @@
+<div class="row">
+	<div class="column">
+		<div class="mobile-padding">
+			<h4 class="bold"><?php the_sub_field( 'title' ); ?></h4>
+			<?php the_sub_field( 'txt' ); ?>
+		</div>
+	</div>
+</div>
